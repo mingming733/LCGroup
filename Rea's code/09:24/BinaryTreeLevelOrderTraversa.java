d0204cfc0b2d916db3f5941a3c64b414b8f4e647
Rea612 <suddenlyjune@gmail.com>
2015-09-24 14:57:25 -0700

    code for 09/24

---
+++ Rea's code/09:24/BinaryTreeLevelOrderTraversa.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+public class BinaryTreeLevelOrderTraversa {
+	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+        //BFS
+        ArrayList <ArrayList <Integer>> ans = new ArrayList <ArrayList <Integer>>();
+        if (root == null){
+            return ans;
+        }
+        Queue <TreeNode> q = new LinkedList <TreeNode>();
+        q.add (root);
+        while (! q. isEmpty()){
+            ans.add (new ArrayList <Integer>());
+            int currSize = q.size();
+            for (int i = 0; i < currSize; i ++){
+                TreeNode node = q. remove();
+                ans.get(ans.size() - 1).add(node.val);
+                if (node.left != null) 
+                    q.add(node.left);
+                if (node.right != null) 
+                    q.add(node.right);
+        }
+    }
+    return ans;
+            }
+}
