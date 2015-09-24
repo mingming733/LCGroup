2eee945484618cc631c474ed17ead8b82570ff80
wangjueolivia <wangjueolivia@users.noreply.github.com>
2015-09-23 20:25:59 -0700

    0922

--- Olivia/stack/valid parentheses.java
+++ Olivia/stack/valid parentheses.java
@@ -23,3 +23,28 @@ public class Solution {
         
     }
 }
+
+//hash map solution
+public static boolean isValid(String s){
+		HashMap<Character,Character> map=new HashMap<>();
+		map.put('(', ')');
+		map.put('[', ']');
+		map.put('{', '}');
+		
+		Stack<Character> stack =new Stack<>();
+		
+		for(int i=0;i<s.length();i++){
+			char c=s.charAt(i);
+			if(map.keySet().contains(c)){
+				stack.push(c);
+			}else if(map.values().contains(c)){
+				if(!stack.empty()&&map.get(stack.peek())==c){
+					stack.pop();
+					
+				}
+				else{return false;
+			}
+		}
+	}return stack.empty();
+
+	}
