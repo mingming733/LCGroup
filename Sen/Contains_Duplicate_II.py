6e16efa57ae99c5c65384598340c16fd91877829
jsongcat <xianmaojohn8811@gmail.com>
2015-10-04 19:55:53 -0700

    10/4/2015

---
+++ Sen/Contains_Duplicate_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {boolean}
+    def containsNearbyDuplicate(self, nums, k):
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                j = d[nums[i]]
+                if i - j <= k:
+                    return True
+            d[nums[i]] = i 
+        return False
